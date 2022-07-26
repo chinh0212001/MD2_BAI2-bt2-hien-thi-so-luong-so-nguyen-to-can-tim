25a6ab05b4e3c71b3d0e8eb89976452d63736088
unknown <duongducchinh160@gmail.com>
2022-07-26 17:08:45 +0700

    so nguyen to

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int count = 0;
+        int j =2;
+
+        System.out.println("Enter the number of prime numbers to find : ");
+        number = scanner.nextInt();
+
+        while (count < number) {
+            if(checkPrime(j)) {
+                System.out.println(j);
+                count ++;
+            }
+            if (count >= number){
+                break;
+            }
+            j++;
+        }
+    }
+    public static boolean checkPrime(int j) {
+        if (j <= 2) {
+            return true;
+        } else {
+            for (int i = 2; i <= Math.sqrt(j); i++) {
+                if (j % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
